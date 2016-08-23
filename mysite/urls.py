abeff33771f7d8a1b3d723acee1a10e73dcd7aa7
LikaG <elizagord@gmail.com>
2016-08-23 22:11:14 +0200

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'http://127.0.0.1:8000/', include('blog.urls')),
+    #url(r'', include('blog.urls')),
 ]
